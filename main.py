2c6402781d1ec5d5da885446969ebfe414980a6b
Mykola <kolia.chaun@icloud.com>
2024-01-10 19:12:24 +0200

    first get endpoint

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print(
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def root() -> dict:
+    return {'try': 'OK'}
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', reload=True, host='0.0.0.0', port=8500)
