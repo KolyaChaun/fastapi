bbb0f07f0b342ce0e13ee2155cd81188579fe01c
Mykola <kolia.chaun@icloud.com>
2024-01-13 14:44:25 +0200

    add db and html

--- constants.py
+++ constants.py
@@ -1,6 +1,12 @@
 from enum import Enum
 
 
-class Gender(str, Enum):
-    WOMEN = 'women'
-    MAN = 'man'
+class TypesOfRecipes(str, Enum):
+    FIRST_MEAL = 'First meal'
+    SECOND_MEAL = 'Second meal'
+    SALAD = 'Salad'
+    SNACKS = 'Snacks'
+    DESSERTS = 'Desserts'
+    SWEET_PASTRIES = 'Sweet pastries'
+    SAUCE = 'Sauce'
+    OTHER = 'Other'
--- storage.py
+++ storage.py
@@ -2,27 +2,28 @@ from abc import ABC, abstractmethod
 import config
 import pymongo
 import certifi
+from uuid import uuid4
 
 
 class BaseStorage:
     @abstractmethod
-    def create_bag(self, bag: dict):
+    def create_recipe(self, bag: dict):
         pass
 
     @abstractmethod
-    def get_bags(self):
+    def get_recipe(self):
+        pass
+
+    # @abstractmethod
+    # def get_recipes_by_title(self):
+    #     pass
+
+    @abstractmethod
+    def update_recipe(self, recipe_uuid: str, new_description: str):
         pass
 
     @abstractmethod
-    def get_bags_by_title(self):
-        pass
-
-    @abstractmethod
-    def update_bag(self):
-        pass
-
-    @abstractmethod
-    def delete_bag(self):
+    def delete_recipe(self, recipe_uuid: str):
         pass
 
 
@@ -35,23 +36,32 @@ class MongoStorage(BaseStorage):
 
         client = pymongo.MongoClient(url, tlsCAFile=certifi.where())
 
-        db = client['Bags']
-        self.collection = db['Bags']
+        db = client['Recipes']
+        self.collection = db['Recipes']
 
-    def create_bag(self, bag: dict):
-        self.collection.insert_one(bag)
+    def create_recipe(self, recipe: dict) -> dict:
+        recipe['uuid'] = str(uuid4())
+        self.collection.insert_one(recipe)
+        return recipe
 
-    def get_bags(self):
-        raise NotImplemented
+    def get_recipe(self, skip: int = 0, limit: int = 10, search_param: str = None):
+        query = {}
+        if search_param:
+            query = {'title': {'$regex': search_param.strip()}}
+        return self.collection.find(query).skip(skip).limit(limit)
 
-    def get_bags_by_title(self):
-        raise NotImplemented
+    # def get_recipes_by_title(self):
+    #     raise NotImplemented
 
-    def update_bag(self):
-        raise NotImplemented
+    def update_recipe(self, recipe_uuid: str, new_description: str):
+        filter_data = {'uuid': recipe_uuid}
+        new_data = {'$set': {'description': new_description}}
+        processed = self.collection.update_one(filter_data, new_data)
+        return processed
 
-    def delete_bag(self):
-        raise NotImplemented
+    def delete_recipe(self, recipe_uuid: str):
+        filter_data = {'uuid': recipe_uuid}
+        self.collection.delete_one(filter_data)
 
 
 storage = MongoStorage()
